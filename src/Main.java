fd69fc580a968f71ef2b0fa9e9a95b6ad18cd8f7
Matheus Silva <scruz.math@gmail.com>
2025-03-20 03:19:11 -0300

    224

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         Basic_Calculator_224 test = new Basic_Calculator_224();
 
-        System.out.println(test.calculate("(1+(4+5+2)-3)+(6+8)"));
+        System.out.println(test.calculate("- (3 + (4 + 5))"));
     }
 }
 
