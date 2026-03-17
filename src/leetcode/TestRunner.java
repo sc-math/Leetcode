4561482fdffc3f62d4fdd9c9a7c04f15cd85950b
Matheus Silva <scruz.math@gmail.com>
2026-03-17 15:49:10 -0300

    Daily - 17/03

--- src/leetcode/TestRunner.java
+++ src/leetcode/TestRunner.java
@@ -1,11 +1,11 @@
 package leetcode;
 
-import leetcode.hash.P0001_TwoSum;
+import leetcode.matrix.P1727_LargestSubmatrixWithRearrangements;
 
 public class TestRunner {
     public static void main(String[] args) {
-        P0001_TwoSum sol = new P0001_TwoSum();
+        P1727_LargestSubmatrixWithRearrangements sol = new P1727_LargestSubmatrixWithRearrangements();
 
-        System.out.println();
+        System.out.println(sol.largestSubmatrix(new int[][]{{0,0,1}, {1,1,1}, {1,0,1}}));
     }
 }
