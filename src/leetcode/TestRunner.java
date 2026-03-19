6e4d119f71dcb284d16fd975aee6490686a88573
Matheus Silva <scruz.math@gmail.com>
2026-03-19 19:57:42 -0300

    Daily - 19/03

--- src/leetcode/TestRunner.java
+++ src/leetcode/TestRunner.java
@@ -1,11 +1,11 @@
 package leetcode;
 
-import leetcode.matrix.P1727_LargestSubmatrixWithRearrangements;
+import leetcode.matrix.P3212_CountSubmatricesEqualFreqXY;
 
 public class TestRunner {
     public static void main(String[] args) {
-        P1727_LargestSubmatrixWithRearrangements sol = new P1727_LargestSubmatrixWithRearrangements();
+        P3212_CountSubmatricesEqualFreqXY sol = new P3212_CountSubmatricesEqualFreqXY();
 
-        System.out.println(sol.largestSubmatrix(new int[][]{{0,0,1}, {1,1,1}, {1,0,1}}));
+        System.out.println(sol.numberOfSubmatrices(new char[][]{{'.', 'X'}, {'.', 'Y'}}));
     }
 }
